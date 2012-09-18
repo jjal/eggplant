5ac94773657c1f8bb8476d1b1be79c090c767b31
futuresandwich <lorenzon@gmail.com>
2012-09-18 12:34:24 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eggplant::Application.config.secret_token = 'f930c93186e2d1d29b010e5e14d87f1ce5783c45ba7556189d58ebcc2aab2de77aae6d3571a1362ef52c107741363e4c05463f211b177d8b93061711cc49a779'
