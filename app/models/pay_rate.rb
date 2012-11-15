4fc01c36aabd338a37907aa2573317382406bf49
futuresandwich <lorenzon@gmail.com>
2012-11-15 14:16:31 +0700

    updating leave calculation

--- app/models/pay_rate.rb
+++ app/models/pay_rate.rb
@@ -1,7 +1,7 @@
 class PayRate < ActiveRecord::Base
   attr_accessible :FTE, :hourly_rate, :id, :monthly_rate, :name, :type
   has_many :employees
-  HOURLY_LEAVE_EARNED = 0.75
+  HOURLY_LEAVE_EARNED = 0.07
   
   S_OPERATIONS_MGR = 1
   S_MECHANIC = 2
